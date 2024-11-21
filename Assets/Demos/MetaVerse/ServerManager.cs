4b6a7a343891daed0fa0621e365cb70c966d7dfe
Victor Huang <huang.victor.mail@gmail.com>
2024-11-21 16:13:36 +0100

    Update UDP

--- Assets/Demos/MetaVerse/ServerManager.cs
+++ Assets/Demos/MetaVerse/ServerManager.cs
@@ -1,27 +1,51 @@
+using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class ServerManager : MonoBehaviour
 {
-    public UDPReceiver Receiver;
+    public UDPService UDP;
+    public int ListenPort = 25000;
 
-    void Awake(){
-    //desactiver l'objet si je ne suis pas le serveur
-        if (!Globals.IsServer){
-        gameObject.SetActive(false);
+    public Dictionary<string, IPEndPoint> Clients = new Dictionary<string, IPEndPoint>(); 
+
+    void Awake() {
+        // Desactiver mon objet si je ne suis pas le serveur
+        if (!Globals.IsServer) {
+            gameObject.SetActive(false);
         }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        Receiver.Listen(
-        (string message) => {
-            Debug.Log("message received from" + message);
-        });
+        UDP.Listen(ListenPort);
+
+        UDP.OnMessageReceived +=  
+            (string message, IPEndPoint sender) => {
+                Debug.Log("[SERVER] Message received from " + 
+                    sender.Address.ToString() + ":" + sender.Port 
+                    + " =>" + message);
+                
+                switch (message) {
+                    case "coucou":
+                        // Ajouter le client à mon dictionnaire
+                        string addr = sender.Address.ToString() + ":" + sender.Port;
+                        if (!Clients.ContainsKey(addr)) {
+                            Clients.Add(addr, sender);
+                        }
+                        Debug.Log("There are " + Clients.Count + " clients present.");
+
+                        UDP.SendUDPMessage("welcome!", sender);
+                        break;
+                }
+                
+                //@todo : do something with the message that has arrived! 
+            };
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
+    public void BroadcastUDPMessage(string message) {
+        foreach (KeyValuePair<string, IPEndPoint> client in Clients) {
+            UDP.SendUDPMessage(message, client.Value);
+        }
     }
 }
